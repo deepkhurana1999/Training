6a6cb0e09f11063c9c280e947677120e5b88d25c
Deep khurana <marverev@gmail.com>
2021-03-23 17:05:10 +0530

    day-8 assignment done

---
+++ Day-8/Deep/consoleapp/Extension.cs
@@ -0,0 +1,16 @@
+﻿using core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleapp
+{
+    static class Extension
+    {
+        public static void display(this Product prod)
+        {
+            Console.WriteLine($"Title: {prod.Title}\nPrice: {prod.Price}\nColor: {prod.Color}");
+        }
+            
+    }
+}
---
+++ Day-8/Deep/consoleapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using core;
+using infrastructure;
+using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory cluster = new Inventory();
+            Console.WriteLine(cluster.ToString());
+            Product monitor = new Product { Title="Monitor", Color = "Black", Price = 15000 };
+            monitor.display();
+        }
+    }
+}
