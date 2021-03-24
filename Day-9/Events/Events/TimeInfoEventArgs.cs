4701a7a288e7305a1796f1972dbff5145ee4a444
Deep khurana <marverev@gmail.com>
2021-03-24 13:05:45 +0530

    day-9 class assignment

---
+++ Day-9/Events/Events/TimeInfoEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    class TimeInfoEventArgs : EventArgs
+    {
+        public int hour;
+        public int minute;
+        public int second;
+        public TimeInfoEventArgs(int h, int m, int s)
+        {
+            this.hour = h;
+            this.minute = m;
+            this.second = s;
+        }
+    }
+}
---
+++ Day-9/PlayerDelegate/PlayerDelegate/VideoPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerDelegate
+{
+    class VideoPlayer
+    {
+        private int videoPlayerStatus;
+        public int PlayVideoFile(string str)
+        {
+            Console.WriteLine("Video Player");
+            if (str != "mp4")
+                videoPlayerStatus = -1;
+            else
+                videoPlayerStatus = 0;
+            return videoPlayerStatus;
+        }
+    }
+}
