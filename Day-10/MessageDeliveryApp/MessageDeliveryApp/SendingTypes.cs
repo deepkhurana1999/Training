3350e79ae996711ea0375006599f0296aa48b4ec
Deep khurana <marverev@gmail.com>
2021-03-25 18:53:11 +0530

    assignment completed

---
+++ Day-10/MessageDeliveryApp/MessageDeliveryApp/SendingTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageDeliveryApp
+{
+     static class SendingTypes
+    {
+        public static bool SendByMail(Message message)
+        {
+            Console.WriteLine($"The message \"{message.Content}\" sent to {message.Address} via Mail.");
+            return true;
+        }
+        public static bool SendByCar(Message message)
+        {
+            Console.WriteLine($"The message \"{message.Content}\" sent to {message.Address} by Car.");
+            return true;
+        }
+        public static bool SendByShip(Message message)
+        {
+            Console.WriteLine($"{message.Content} could not deliverd to {message.Content} by Ship");
+            return false;
+        }
+    }
+}
