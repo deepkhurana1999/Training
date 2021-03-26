db2c7281759f8fc62492f7c0ef9d597a3fe18bfa
Deep khurana <marverev@gmail.com>
2021-03-26 14:22:36 +0530

    day-11 class lab A simple expression tree

---
+++ Day-11/SimpleExpressionTree/SimpleExpressionTree/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace SimpleExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Linq.Expressions.Expression<Func<int, int>> expressionTree = x => x * x;
+            Console.WriteLine("Expression Tree:");
+            Console.WriteLine(expressionTree);
+            var a = expressionTree.Compile();
+            Console.WriteLine("Result: "+ a(8));
+        }
+    }
+}
