131621fbdfa04af5f6cee8023e8dbf357da2a6e3
Deep khurana <marverev@gmail.com>
2021-03-28 21:00:31 +0530

    Lab 8: overriding ToString() method

---
+++ C#/Lab_8/OverrideToStringMethod/OverrideToStringMethod/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverrideToStringMethod
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public override string ToString()
+        {
+            return $"ID: {Id}\nTitle: {Title}\nPrice: {Price}";
+        }
+    }
+}
---
+++ C#/Lab_8/OverrideToStringMethod/OverrideToStringMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OverrideToStringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product car = new Product
+            {
+                Id = 1,
+                Title = "Dodge Charger Dayota",
+                Price = 25
+            };
+
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
