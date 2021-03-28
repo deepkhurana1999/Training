d71681af70203e036a090e3392b4fe403bd61ae4
Deep khurana <marverev@gmail.com>
2021-03-28 19:38:04 +0530

    Lab 4:  Creating a Calculator App with Delegates

---
+++ C#/Lab_4/CalculatorDelegate/CalculatorDelegate/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorDelegate
+{
+    class Calculator
+    {
+        public delegate int ALU();
+        
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public int Addition()
+        {
+            return Num1 + Num2;
+        }
+        public int Subtraction()
+        {
+            return Num1 - Num2;
+        }
+        public int Multiplication()
+        {
+            return Num1 * Num2;
+        }
+        public int Division()
+        {
+            return Num1 / Num2;
+        }
+        public void Print(ALU alu)
+        {
+            Console.WriteLine(alu());
+        }
+    }
+}
