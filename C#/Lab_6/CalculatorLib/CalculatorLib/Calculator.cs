33b1404c4ba11257ca2e5fb1d59e207516596415
Deep khurana <marverev@gmail.com>
2021-03-28 20:08:57 +0530

    Lab 6: Creating an Extension Method to display product where original class is in different project and class definition is in different project. Idea is to understand an Extension Method does not need recompilation of original class

---
+++ C#/Lab_6/CalculatorLib/CalculatorLib/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public int Addition()
+        {
+            return this.Num1 + this.Num2;
+        }
+        public int Subtraction()
+        {
+            return this.Num1 + this.Num2;
+        }
+    }
+}
---
+++ C#/Lab_6/ExtensionMethod/ExtensionMethod/ExtensionClass.cs
@@ -0,0 +1,44 @@
+﻿using CalculatorLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethod
+{
+    static class ExtensionClass
+    {
+        public static int VowelCount(this String str)
+        {
+            int result = 0;
+            String strCpy = str.ToLower();
+            for (int i = 0; i < strCpy.Length; i++)
+            {
+                switch (strCpy[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u': result++; break;
+                }
+            }
+            return result;
+        }
+
+        public static int Multiplication(this Calculator calc)
+        {
+            return calc.Num1 * calc.Num2;
+        }
+
+        public static int Division(this Calculator calc)
+        {
+            return calc.Num1 / calc.Num2;
+        }
+
+        public static int Negate(this int i)
+        {
+            return -i;
+        }
+
+    }
+}
