0da059780ee7004e002581ca22378d1e98812364
Deep khurana <marverev@gmail.com>
2021-03-28 17:24:55 +0530

    Lab-2: Banking Example which is Implementing C-Sharp class

---
+++ C#/Lab_2/BankingTransactions/BankingTransactions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BankingTransactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deepAccount = new BankAccount("Customer Name", 20000);
+            deepAccount.MakeDeposit(500, DateTime.Now, "Initial Deposit");
+            try
+            {
+                deepAccount.WithDraw(50, DateTime.Now, "1st Withdraw");
+                Console.WriteLine(deepAccount.GetAccount());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+
+
+            var customerAccount = new BankAccount("Customer", 2000);
+            customerAccount.MakeDeposit(1000, DateTime.Now, "Initial Deposit");
+            try
+            {
+                customerAccount.WithDraw(100, DateTime.Now, "Final Withdraw");
+                Console.WriteLine(customerAccount.GetAccount());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
