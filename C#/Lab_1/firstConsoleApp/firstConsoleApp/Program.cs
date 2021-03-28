88d71f9c2bdca1ae3c2fd70c460cff66dd3cbea0
Deep khurana <marverev@gmail.com>
2021-03-28 17:21:11 +0530

    Lab-1: Hello World

---
+++ C#/Lab_1/firstConsoleApp/firstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace firstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"Hello {name.ToUpper()} on {date}");
+        }
+    }
+}
