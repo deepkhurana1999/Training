0bbd74f69d338f2ceb11297ff9aa151295fd0b53
Deep khurana <marverev@gmail.com>
2021-03-28 20:47:48 +0530

    Lab 7: Count Vowels Extension Method Demo

---
+++ C#/Lab_7/VowelCounter/VowelCounter/ExtensionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VowelCounter
+{
+    static class ExtensionClass
+    {
+        public static int VowelCount(this String str)
+        {
+            int result = 0;
+            String strCpy = str.ToLower();
+            for (int i = 0; i < strCpy.Length; i++)
+            {
+                switch (strCpy[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u': result++; break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ C#/Lab_7/VowelCounter/VowelCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of Extension Method:");
+            string title = "Extension Method";
+
+            Console.Write($"Number of vowels in the string \"{title}\": ");
+            Console.WriteLine(title.VowelCount());
+        }
+    }
+}
