e17f260e44f7f811a10845db5d3289c32300aeb5
Deep khurana <marverev@gmail.com>
2021-03-29 14:50:02 +0530

    Lab 12: Calculator with delegate print output

---
+++ C#/Lab_12/CalculatorDelegate/CalculatorDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculatorDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator
+            {
+                Num1 = 10,
+                Num2 = 5
+            };
+
+            Console.WriteLine("Arithmetic Operations of two integers:\nNumber1: 10\tNumber2: 5\n");
+
+            Calculator.ALU alu = calc.Addition;
+            Console.Write("Addition: "); calc.Print(alu);
+
+            alu = new Calculator.ALU(calc.Subtraction);
+            Console.Write("Subtraction: "); calc.Print(alu);
+
+            alu = new Calculator.ALU(calc.Multiplication);
+            Console.Write("Multiplication: "); calc.Print(alu);
+
+            alu = new Calculator.ALU(calc.Division);
+            Console.Write("Division: ");   calc.Print(alu);
+        }
+    }
+}
