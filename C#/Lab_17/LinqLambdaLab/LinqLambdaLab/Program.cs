9aa9f7103148f1935afcc54842240a5a4148863f
Deep khurana <marverev@gmail.com>
2021-03-29 15:46:52 +0530

    Lab 17: A demo on LINQ zip operator

---
+++ C#/Lab_17/LinqLambdaLab/LinqLambdaLab/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqLambdaLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    ID=1,
+                    Title="Car",
+                    Price=100
+                },
+                new Product
+                {
+                    ID = 2,
+                    Title = "Pen",
+                    Price = 200
+                },
+                new Product
+                {
+                    ID = 3,
+                    Title = "Pencil",
+                    Price=300
+                },
+                new Product
+                {
+                    ID = 4,
+                    Title="MotorCycle",
+                    Price=500
+                }
+            };
+
+
+            List<Distributor> distributors = new List<Distributor>
+            {
+                new Distributor
+                {
+                    ID=1,
+                    ProductID=1,
+                    Name="ABC"
+                },
+                new Distributor
+                {
+                    ID=2,
+                    ProductID=2,
+                    Name="XYZ"
+                },
+                new Distributor
+                {
+                    ID=3,
+                    ProductID=3,
+                    Name="JHL"
+                },
+                new Distributor
+                {
+                    ID=4,
+                    ProductID=4,
+                    Name="QWE"
+                }
+            };
+
+            Console.WriteLine($"ProductID\tDistributor Name");
+            var mergeList = products.Zip(distributors, (p, d) => new { ProductID=p.ID, DistributorName=d.Name });
+            mergeList.ToList().ForEach(x => Console.WriteLine($"{x.ProductID}\t\t{x.DistributorName}"));
+            
+        }
+
+    }
+}
