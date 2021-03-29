5efc4e04d0c081530658dea1bd99578201c68445
Deep khurana <marverev@gmail.com>
2021-03-29 15:49:03 +0530

    Lab 18: Creating a generics class, using Action to pass function at run time

---
+++ C#/Lab_18/GenericClassAndAction/GenericClassAndAction/Cal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClassAndAction
+{
+    class Cal<T>
+    {
+        public T echo { get; set; }
+        public void Display(Action displayType)
+        {
+            displayType();
+        }
+
+    }
+}
---
+++ C#/Lab_18/GenericClassAndAction/GenericClassAndAction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GenericClassAndAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cal<int> echInt = new Cal<int>
+            {
+                echo = 9
+            };
+
+            Cal<Product> echProduct = new Cal<Product>
+            {
+                echo = new Product
+                {
+                    ID = 4,
+                    Title = "MotorCycle",
+                    Price = 500
+                }
+            };
+
+            Action intDisplay = () => { Console.WriteLine($"Square of 9: {echInt.echo * echInt.echo}."); };
+            Action productDisplay = () => { Console.WriteLine($"Price of {echProduct.echo.Title} is {echProduct.echo.Price}."); };
+
+            echInt.Display(intDisplay);
+            echInt.Display(productDisplay);
+        }
+    }
+}
