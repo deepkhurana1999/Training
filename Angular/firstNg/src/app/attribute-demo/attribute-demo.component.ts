edcbebbbd8933005a8057e8904f445c59595fbff
Deep khurana <marverev@gmail.com>
2021-04-07 15:09:59 +0530

    commit

--- Angular/firstNg/src/app/attribute-demo/attribute-demo.component.ts
+++ Angular/firstNg/src/app/attribute-demo/attribute-demo.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AttributeDemoComponent implements OnInit {
 
   constructor() { }
-  color: string;
+  color: string = 'black';
   ngOnInit(): void {
   }
 
